6f0843b2172ce2ce129cfdecd0410afd7e71b6b2
luoyunpeng <lyp@qq.com>
2024-07-30 16:55:35 +0800

    优化

--- src/components/MarkdownEditor/index.vue
+++ src/components/MarkdownEditor/index.vue
@@ -3,12 +3,9 @@
 </template>
 
 <script>
-// deps for editor
-import 'codemirror/lib/codemirror.css' // codemirror
-import 'tui-editor/dist/tui-editor.css' // editor ui
-import 'tui-editor/dist/tui-editor-contents.css' // editor content
-
-import Editor from 'tui-editor'
+import 'codemirror/lib/codemirror.css'
+import '@toast-ui/editor/dist/toastui-editor.css'
+import Editor from '@toast-ui/editor'
 import defaultOptions from './default-options'
 
 export default {
